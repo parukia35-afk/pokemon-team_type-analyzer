2193f991707692a8abb9c016b8cf486c83aa3c3f
(Turquoise)parukia35-afk <parukia35@gmail.com>
2026-05-27 18:37:07 +0800

    style: 安裝並設定 Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import AnalyzerWrapper from './components/AnalyzerWrapper'
+
+function App() {
+  
+
+  return (
+    <div className='min-h-screen w-full flex justify-center p-8 bg-black'>
+      <AnalyzerWrapper></AnalyzerWrapper>
+    </div>
+  )
+}
+
+export default App
