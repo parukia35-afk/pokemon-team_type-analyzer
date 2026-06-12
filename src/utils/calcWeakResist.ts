6109b617c01754d133764111e7a7a0087ae248e9
(Turquoise)parukia35-afk <parukia35@gmail.com>
2026-06-12 17:12:57 +0800

    fix(calcWeakResist.ts): 修正弱點與抗性應優先抵銷免疫
    - 第一屬性的弱點應先和第二屬性的抗性抵銷(本來沒有) (第二屬性同理)ex.海兔獸
    - 第一屬性的抗性和第二屬性的免疫如果重複應只顯示在免疫就好，避免抗性再次計算(第二屬性同理) ex.泥巴魚

--- src/utils/calcWeakResist.ts
+++ src/utils/calcWeakResist.ts
@@ -26,19 +26,34 @@ function lookupTeamTypes(members: Types[]) {
         secondType.immune = (typeChart as Record<string, TypeData>)[type].immune;
       }
     });
-    // 第一屬性的弱點與第二屬性抗性抵銷
-    const firstTypeWeakness = firstType.weak.filter((value) => {
+    // 先將第一屬性弱點和第二屬性的免疫抵銷，第二屬性同理
+    const firstWeakMinusSecondImmune = firstType.weak.filter((value)=>{
+      return !secondType.immune.includes(value)
+    })
+    const secondWeakMinusFirstImmune = secondType.weak.filter((value)=>{
+      return !firstType.immune.includes(value)
+    })
+    // 第一屬性的抗性如果和第二屬性的免疫重疊，那麼第一屬性的抗性要移除掉，只留在免疫就好，避免重複計算，第二屬性同理
+    const firstResistMinusSecondImmune = firstType.resist.filter((value)=>{
+      return !secondType.immune.includes(value)
+    })
+    const secondResistMinusFirstImmune = secondType.resist.filter((value)=>{
+      return !firstType.immune.includes(value)
+    })
+
+    // 第一屬性的弱點與第二屬性抗性抵銷，算出最終弱點和抗性
+    const firstTypeWeakness = firstWeakMinusSecondImmune.filter((value) => {
       return !secondType.resist.includes(value);
     });
-    const secondTypeWeakness = secondType.weak.filter((value) => {
+    const secondTypeWeakness = secondWeakMinusFirstImmune.filter((value) => {
       return !firstType.resist.includes(value);
     });
     const weakness = firstTypeWeakness.concat(secondTypeWeakness); // 最終弱點
 
-    const firstTypeResistance = firstType.resist.filter((value) => {
+    const firstTypeResistance = firstResistMinusSecondImmune.filter((value) => {
       return !secondType.weak.includes(value);
     });
-    const secondTypeResistance = secondType.resist.filter((value) => {
+    const secondTypeResistance = secondResistMinusFirstImmune.filter((value) => {
       return !firstType.weak.includes(value);
     });
     const resistance = firstTypeResistance.concat(secondTypeResistance); // 最終抗性
